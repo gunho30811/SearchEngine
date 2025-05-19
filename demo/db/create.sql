804c8e1f58274a98b477ab51917d21ce21510da1
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2025-05-19 09:40:52 +0900

    db insert, create sql

---
+++ demo/db/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE kostatSample (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    source VARCHAR(255) NOT NULL,
+    created_at CHAR(14) NOT NULL,
+);
